fd26a5ee6e39245b50df0bc6fa4e2ecbd4a9c3be
李波 <tolibo@qq.com>
2018-09-14 00:36:02 +0800

    fixed

--- handlers/proxy.js
+++ handlers/proxy.js
@@ -191,7 +191,9 @@ async function proxyHandler (request, response, next) {
         ) {
             _write.call(response, data);
         } else {
-            _write.call(response, data.toString().replace(/http:\/\/www.360zhijia.com\//gi, "https://www.360zhijia.com/"));
+            _write.call(response, data.toString()
+                .replace(/http:\/\/www.360zhijia.com\//gi, "https://www.360zhijia.com/"))
+                .replace(/begin\/timthumb.php\?src=https:\/\/www.360zhijia.com/gi, "begin/timthumb.php?src=");
         }
     };
 

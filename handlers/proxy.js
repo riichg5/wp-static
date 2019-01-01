5352032ac15981701aa1b9b91040b2132c097a14
李波 <tolibo@qq.com>
2019-01-01 19:51:02 +0800

    优化response

--- handlers/proxy.js
+++ handlers/proxy.js
@@ -145,8 +145,6 @@ function processAds (opts) {
     let request = opts.request;
     let adsConfig = _config.get("ads");
 
-    console.log(`-> adsConfig: ${JSON.stringify(adsConfig)}`);
-
     if(!adsConfig || !adsConfig.isOn) {
         return html;
     }
@@ -205,9 +203,15 @@ async function proxyHandler (request, response, next) {
                 return;
             }
 
+            //只从静态文件里面抓取content-type返回
             let responseInfo = JSON.parse(data);
-            for(let key in responseInfo.headers) {
-                response.setHeader(key, responseInfo.headers[key]);
+            let oldHeader = responseInfo.headers;
+
+            if(oldHeader) {
+                let contentType = oldHeader['content-type'];
+                if(contentType) {
+                    response.setHeader('content-type', contentType);
+                }
             }
 
             responseInfo.html = responseInfo.html.replace(/http:\/\/www.360zhijia.com\//gi, "https://www.360zhijia.com/");
@@ -215,29 +219,6 @@ async function proxyHandler (request, response, next) {
                 html: responseInfo.html,
                 request: request
             });
-
-            // //如果是PC端，投放标题上面的链接广告
-            // if(isPcClient(request)) {
-            //     // responseInfo.html = responseInfo.html.replace(`autoptimize_4038f49b0ca942d54e086868e610f7d6.css`, `autoptimize_4038f49b0ca942d54e086868e610f7d6_v1.css`);
-            //     responseInfo.html = responseInfo.html.replace(/(autoptimize_)\S+(\.css)/, `autoptimize_4038f49b0ca942d54e086868e610f7d6_v2.css`);
-            //     responseInfo.html = responseInfo.html.replace(`<header class="entry-header">`, `
-            //         <div class="entry-header header-linkad">
-            //             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            //                 <!-- PC文章标题上部 -->
-            //                 <ins class="adsbygoogle" style="display:inline-block;width:728px;height:17px" data-ad-client="ca-pub-0044506972792760" data-ad-slot="5853086007"></ins>
-            //             <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-            //         </div>
-            //         <header class="entry-header entry-header-notop">
-            //     `);
-            // } else { //移动端顶部文字链接广告
-            //     responseInfo.html = responseInfo.html.replace(/(autoptimize_)\S+(\.css)/, `autoptimize_4038f49b0ca942d54e086868e610f7d6_v2.css`);
-            //     responseInfo.html = responseInfo.html.replace(`<header class="entry-header">`, `
-            //         <div class="entry-header header-linkad">
-            //         ${}
-            //         </div>
-            //         <header class="entry-header entry-header-notop">
-            //     `);
-            // }
 
             if(!isUCBrowser) {
                 response.end(responseInfo.html);
